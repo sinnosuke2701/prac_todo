922bfd58357a2b11a04d70cfcaa4292d2943516b
sinnosuke2701 <ehgml1111@naver.com>
2024-09-11 22:23:45 +0900

    todo

--- todo/src/main/java/com/shinnosuke/infra/todo/ToDoController.java
+++ todo/src/main/java/com/shinnosuke/infra/todo/ToDoController.java
@@ -1,7 +1,14 @@
 package com.shinnosuke.infra.todo;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ToDoController {
 	
-	
+	@RequestMapping(value = "/xdm/v1/infra/todo/ToDoList")
+	public String ToDoList() {
+		return "/xdm/v1/infra/todo/ToDoList";
+	}
 	
 }
